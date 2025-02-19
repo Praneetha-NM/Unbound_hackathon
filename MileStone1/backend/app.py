8e88c63c8bea53411f34c48bb31afe9bc8d2c3a7
Praneetha-NM <praneetha7597@gmail.com>
2025-02-19 11:17:39 +0530

    Add config.py

--- MileStone1/backend/app.py
+++ MileStone1/backend/app.py
@@ -1,22 +1,21 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 import psycopg2
-from dotenv import load_dotenv
 import os
+from config import Config
 
-load_dotenv()
 app = Flask(__name__)
+app.config.from_object(Config)
 CORS(app)
 
 # Database connection
 def connect_db():
-    return psycopg2.connect(
-        dbname=os.getenv("DB_NAME"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        host=os.getenv("DB_HOST"),
-        port=os.getenv("DB_PORT")
-    )
+    try:
+        return psycopg2.connect(Config.SQLALCHEMY_DATABASE_URI)
+    except psycopg2.Error as e:
+        print("Database connection error:", e)
+        return None
+
 
 @app.route('/models', methods=['GET'])
 def get_models():
